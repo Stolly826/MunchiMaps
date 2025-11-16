1f9325e956eb112f9a21e7d4226b04a9d3f41e5b
Stolly826 <devonbecker05@gmail.com>
2025-11-15 20:39:50 -0500

    Campus Pathing Start
    Began making a system to find paths across campus, mapping out each crossroad in the paths on campus to make a graph that we can then use our algorithms on.

---
+++ algorithm/shortest_path/pathToMachine.py
@@ -0,0 +1,9 @@
+import heapdict
+from munchi_maps_model import MunchiMaps_model
+
+def loadGraph():
+    # CREATE DICT OF EDGES
+def closestNode():
+    # FIND THE NODE CLOSEST TO THE USER TO START THE PATH
+def dijkstra():
+    # FIND SHORTEST PATH ACROSS THE GRAPH CREATED
